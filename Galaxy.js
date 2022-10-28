22eec9d611f1786265c1c21c485c0ce3e452a310
RubenRGanga <rubenruizganga@gmail.com>
2022-10-28 03:49:24 +0200

    9 Alert Controles

--- Galaxy.js
+++ Galaxy.js
@@ -1,3 +1,16 @@
 window.onload = function () {
+    alert("DISPARO = Ctrl \nMOVERSE = Flechas Arriba/Abajo")
+    var myAudio= document.createElement('audio');
+    var myMessageAlert = "";
+    myAudio.src = 'assets/snd/DUST.mp3';
+    myAudio.addEventListener('ended', function(){
+       alert(myMessageAlert);
+    });
+   function Myalert(message) { 
+       myAudio.play();
+       myMessageAlert = message;
+   } 
+   Myalert("DISPARO = Ctrl - MOVERSE = Flechas Arriba/Abajo");
+
     Game.init()
 }
